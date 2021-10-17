8d7a8c50d43b3ccc2630084097094de6a6ddd5bb
zubairahmedanas <amianasahmed@gmail.com>
2021-10-17 20:35:07 +0600

    1.models added 2. templates added 3. views added

---
+++ store/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from .models import Category, Product
+
+
+def categories(request):
+    return {
+        'categories': Category.objects.all()
+    }
+
+
+def all_products(request):
+    products = Product.objects.all()
+    return render(request, 'store/home.html', {'products': products})
